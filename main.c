bf2b6d0a1fd75d3e3fd87450a1077c4af908915b
Maciej Halber <macikuh@gmail.com>
2019-12-29 13:01:44 +0100

    Simple version of OIT working

--- main.c
+++ main.c
@@ -14,9 +14,49 @@
 
 #include "gl_utils.h"
 
+/// INPUT
+typedef struct mouse_state
+{
+  msh_vec2_t prev_pos;
+  msh_vec2_t cur_pos;
+  bool button[3];
+  float scroll;
+} mouse_state_t;
+
+static bool oit_active = true;
+static mouse_state_t mouse = {0};
+
+void scroll_callback( GLFWwindow* window, double xoffset, double yoffset )
+{
+  mouse.scroll = yoffset;
+}
+
+void cursor_pos_callback( GLFWwindow* window, double xpos, double ypos )
+{
+  mouse.prev_pos = mouse.cur_pos;
+  mouse.cur_pos.x = xpos;
+  mouse.cur_pos.y = ypos;
+}
+
+void mouse_button_callback( GLFWwindow* window, int button, int action, int mods)
+{
+  if( action == GLFW_PRESS )
+  {
+    if( button < 3 ) mouse.button[ button ] = 1;
+  }
+  if( action == GLFW_RELEASE )
+  {
+    if( button < 3 ) mouse.button[ button ] = 0;
+  }
+}
 
 void key_callback( GLFWwindow* window, int key, int scancode, int action, int mods )
 {
+  if( key == GLFW_KEY_SPACE && action == GLFW_PRESS ) 
+  {
+    oit_active = !oit_active;
+    printf("TEST %d\n", (int)oit_active);
+  }
 }
 
 int32_t
@@ -46,6 +86,9 @@ main(int32_t argc, char **argv)
   }
 
   glfwSetKeyCallback( window, key_callback );
+  glfwSetScrollCallback( window, scroll_callback );
+  glfwSetMouseButtonCallback( window, mouse_button_callback );
+  glfwSetCursorPosCallback( window, cursor_pos_callback );
   glfwMakeContextCurrent(window);
   
   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
@@ -84,15 +127,21 @@ main(int32_t argc, char **argv)
     GL_UTILS_SHDR_VERSION
     GL_UTILS_SHDR_SOURCE
     (
-      layout(location = 0) uniform sampler2D accum_tex; \n
-      layout(location = 1) uniform sampler2D reveal_tex;  \n
-      out vec4 frag_col;  \n
-      in vec2 v_uv;  \n
-      void main() \n
-      { \n
-        // vec3 col = v_uv.y * vec3(1.0, 0.0, 0.0) + (1.0 - v_uv.y) * vec3(0.0, 0.0, 0.8); \n
-        // frag_col = vec4( 0.5 * texture( accum_tex, v_uv ).rgb + 0.5 * col, 1.0) ; \n
-        frag_col = vec4( texture( reveal_tex, v_uv ).rrr, 1.0 );
+      layout(location = 0) uniform sampler2D accum_tex;
+      layout(location = 1) uniform sampler2D reveal_tex;
+      out vec4 frag_col;
+      in vec2 v_uv;
+      void main() 
+      {
+        // float val = texture( reveal_tex, v_uv ).r;
+        // frag_col = vec4( val, val, val, 1.0 );
+        // frag_col = vec4( texture( accum_tex, v_uv ).rgb, 1.0);
+
+        float revealage = texture( reveal_tex, v_uv ).r;
+        if (revealage == 1.0) { discard; }
+        vec4 accum = texture( accum_tex, v_uv );
+        vec3 avg_col = accum.rgb / clamp( accum.a, 1e-6, 5e4 );
+        frag_col = vec4( avg_col, 1.0 - revealage );
       }
     );
   
@@ -145,10 +194,12 @@ main(int32_t argc, char **argv)
       layout( location = 0 ) out vec4 accum; \n
       layout( location = 1 ) out float revelage; \n
       void main() \n
-      { \n
-        float a = v_col.a; \n
-        float w = clamp(pow(min(1.0, a * 10.0) + 0.01, 3.0) * 1e8 * pow(1.0 - gl_FragCoord.z * 0.9, 3.0), 1e-2, 3e3); \n
-        accum = vec4( v_col.rgb * v_col.a, v_col.a );// * w; \n
+      {
+        // Modified Equation 9 - factor changed to 1.0/2.0 from 1.0/200.0
+        float factor = 1.0/2.0; 
+        float depth = factor * abs(1.0/gl_FragCoord.w);
+        float weight = v_col.a * clamp( (0.03 / (1e-5 + pow(depth, 4.0) ) ), 1e-6, 3e3 );
+        accum = vec4( v_col.rgb, 1.0 ) * weight;
         revelage = v_col.a;
       }
     );
@@ -176,6 +227,59 @@ main(int32_t argc, char **argv)
   glDeleteShader( transparency_vertex_shader );
   glDeleteShader( transparency_fragment_shader );
 
+
+/////// Standard Shader (requires sorting)
+  const char* sorted_vs_src = 
+    GL_UTILS_SHDR_VERSION
+    GL_UTILS_SHDR_SOURCE
+    (
+      layout( location = 0 ) in vec3 pos;
+      layout( location = 1 ) in vec4 col;
+
+      layout( location = 0 ) uniform mat4 mvp;
+      out vec4 v_col;
+      void main()
+      {
+        v_col = col;
+        gl_Position = mvp * vec4( pos, 1.0 );
+      }
+    );
+
+  const char* sorted_fs_src = 
+    GL_UTILS_SHDR_VERSION
+    GL_UTILS_SHDR_SOURCE
+    (
+      in vec4 v_col; \n
+      out vec4 frag_col; \n
+      void main() \n
+      {
+        frag_col = v_col;
+      }
+    );
+  
+  GLuint sorted_vertex_shader = glCreateShader( GL_VERTEX_SHADER );
+  GLuint sorted_fragment_shader = glCreateShader( GL_FRAGMENT_SHADER );
+
+  glShaderSource( sorted_vertex_shader, 1, &sorted_vs_src, 0 );
+  glCompileShader( sorted_vertex_shader );
+  gl_utils_assert_shader_compiled( sorted_vertex_shader, "SORTED VERTEX SHADER" );
+
+  glShaderSource( sorted_fragment_shader, 1, &sorted_fs_src, 0 );
+  glCompileShader( sorted_fragment_shader );
+  gl_utils_assert_shader_compiled( sorted_fragment_shader, "SORTED FRAGMENT SHADER" );
+  
+  GLuint sorted_program_id = glCreateProgram();
+  glAttachShader( sorted_program_id, sorted_vertex_shader );
+  glAttachShader( sorted_program_id, sorted_fragment_shader );
+  
+  glLinkProgram( sorted_program_id );
+  
+  glDetachShader( sorted_program_id, sorted_vertex_shader );
+  glDetachShader( sorted_program_id, sorted_fragment_shader );
+
+  glDeleteShader( sorted_vertex_shader );
+  glDeleteShader( sorted_fragment_shader );
+
 ////////
 
  int32_t binding_idx = 0;
@@ -202,28 +306,28 @@ main(int32_t argc, char **argv)
 /////// Transparent quads geometry setup 
   float quads_verts[] = 
   {
-    -1.0, -1.0,  0.0,   0.2, 0.4, 1.0, 0.5,
-    -1.0,  1.0,  0.0,   0.2, 0.4, 1.0, 0.5,
-     1.0,  1.0,  0.0,   0.2, 0.4, 1.0, 0.5,
-     1.0, -1.0,  0.0,   0.2, 0.4, 1.0, 0.5,
+    -1.0, -1.0,  0.0,   0.0, 0.0, 1.0, 0.75,
+    -1.0,  1.0,  0.0,   0.0, 0.0, 1.0, 0.75,
+     1.0,  1.0,  0.0,   0.0, 0.0, 1.0, 0.75,
+     1.0, -1.0,  0.0,   0.0, 0.0, 1.0, 0.75,
 
-    -1.2, -1.2,  0.2,   1.0, 0.2, 0.3, 0.5,
-    -1.2,  0.8,  0.2,   1.0, 0.2, 0.3, 0.5,
-     0.8,  0.8,  0.2,   1.0, 0.2, 0.3, 0.5,
-     0.8, -1.2,  0.2,   1.0, 0.2, 0.3, 0.5,
-    
-    -0.8, -0.8, -0.2,   0.4, 1.0, 0.1, 0.5,
-    -0.8,  1.2, -0.2,   0.4, 1.0, 0.1, 0.5,
-     1.2,  1.2, -0.2,   0.4, 1.0, 0.1, 0.5,
-     1.2, -0.8, -0.2,   0.4, 1.0, 0.1, 0.5,
+    -1.0, -1.0, -2.0,   1.0, 0.0, 0.0, 0.75,
+    -1.0,  1.0, -2.0,   1.0, 0.0, 0.0, 0.75,
+     1.0,  1.0, -2.0,   1.0, 0.0, 0.0, 0.75,
+     1.0, -1.0, -2.0,   1.0, 0.0, 0.0, 0.75,
+
+    -1.0, -1.0,  -1.0,   1.0, 1.0, 0.0, 0.75,
+    -1.0,  1.0,  -1.0,   1.0, 1.0, 0.0, 0.75,
+     1.0,  1.0,  -1.0,   1.0, 1.0, 0.0, 0.75,
+     1.0, -1.0,  -1.0,   1.0, 1.0, 0.0, 0.75,
   };
 
-
   uint16_t quads_ind[] = { 0, 1, 2,  0, 2, 3,
-                          4, 5, 6,  4, 6, 7,
-                          8, 9, 10, 8, 10, 11 };
-  GLuint quads_vao, quads_vbo, quads_ebo;
+                           4, 5, 6,  4, 6, 7,
+                           8, 9, 10, 8, 10, 11 };
+  GLuint quads_vao, quads_oit_vao, quads_vbo, quads_ebo;
 
+  glCreateVertexArrays( 1, &quads_oit_vao );
   glCreateVertexArrays( 1, &quads_vao );
   glCreateBuffers( 1, &quads_vbo );
   glCreateBuffers( 1, &quads_ebo );
@@ -231,8 +335,19 @@ main(int32_t argc, char **argv)
   glNamedBufferData( quads_vbo, sizeof(quads_verts), quads_verts, GL_STATIC_DRAW );
   glNamedBufferData( quads_ebo, sizeof(quads_ind), quads_ind, GL_STATIC_DRAW );
 
+  glVertexArrayVertexBuffer( quads_oit_vao, binding_idx, quads_vbo, 0, 7 * sizeof( float ) );
+  glVertexArrayElementBuffer( quads_oit_vao, quads_ebo );
+
+  glEnableVertexArrayAttrib( quads_oit_vao, 0 );
+  glEnableVertexArrayAttrib( quads_oit_vao, 1 );
+
+  glVertexArrayAttribFormat( quads_oit_vao, 0, 3, GL_FLOAT, GL_FALSE, 0 );
+  glVertexArrayAttribFormat( quads_oit_vao, 1, 4, GL_FLOAT, GL_FALSE, 3 * sizeof(float) );
+
+  glVertexArrayAttribBinding( quads_oit_vao, 0, binding_idx );
+  glVertexArrayAttribBinding( quads_oit_vao, 1, binding_idx );
+
   glVertexArrayVertexBuffer( quads_vao, binding_idx, quads_vbo, 0, 7 * sizeof( float ) );
-  glVertexArrayElementBuffer( quads_vao, quads_ebo );
 
   glEnableVertexArrayAttrib( quads_vao, 0 );
   glEnableVertexArrayAttrib( quads_vao, 1 );
@@ -280,19 +395,20 @@ main(int32_t argc, char **argv)
 
 //////// Camera setup
   msh_camera_t cam = {0};
-  msh_camera_init(&cam, &(msh_camera_desc_t){.eye = msh_vec3( 3.0f, 3.0f, 6.0f ),
+  msh_camera_init(&cam, &(msh_camera_desc_t){.eye = msh_vec3( -2.0f, -1.2f, 3.0f ),
                                              .center = msh_vec3_zeros(),
                                              .up = msh_vec3_posy(),
                                              .viewport = msh_vec4(0.0f, 0.0f, window_width, window_height),
                                              .fovy = msh_rad2deg(60),
                                              .znear = 0.01f,
-                                             .zfar = 100.0f,
-                                             .use_ortho = true });
+                                             .zfar = 500.0f,
+                                             .use_ortho = false });
   msh_mat4_t mvp = msh_mat4_mul(cam.proj, cam.view);
 
 
- 
-  // glDisable( GL_DEPTH_TEST );
+  glEnable( GL_DEPTH_TEST );
+  glEnable( GL_BLEND );
+  glEnable( GL_FRAMEBUFFER_SRGB );
   while (!glfwWindowShouldClose(window))
   {
     // Update the camera
@@ -302,54 +418,98 @@ main(int32_t argc, char **argv)
       cam.viewport.z = window_width;
       cam.viewport.w = window_height;
       msh_camera_update_proj(&cam);
-      mvp = msh_mat4_mul(cam.proj, cam.view);
       int32_t fb_w, fb_h;
       glfwGetFramebufferSize( window, &fb_w, &fb_h );
     }
-    
+    if( mouse.button[0] == 1 )
+    {
+      msh_camera_rotate( &cam, mouse.prev_pos, mouse.cur_pos );
+      msh_camera_update_view( &cam );
+    }
 
-    
-    glBindFramebuffer( GL_FRAMEBUFFER, oit_fbo );
-    glViewport( 0, 0, 1024, 720 );
-    glEnable( GL_BLEND );
-    
-    static const float clear_accum[] = { 0, 0, 0, 0 };
-    glClearBufferfv(GL_COLOR, 0, clear_accum);
-    glBlendEquationi(0, GL_FUNC_ADD);
-    glBlendFunci( 0, GL_ONE, GL_ONE );
+    if( mouse.scroll != 0 )
+    {
+      msh_camera_zoom( &cam, mouse.scroll );
+      msh_camera_update_view( &cam );
+      mouse.scroll = 0.0f;
+    }
 
-    static const float clear_reveal[] = { 1, 0, 0, 0 };
-    glClearBufferfv(GL_COLOR, 1, clear_reveal);
-    glBlendEquationi(1, GL_FUNC_ADD);
-    glBlendFunci( 0, GL_ZERO, GL_ONE_MINUS_SRC_COLOR );
+    mvp = msh_mat4_mul(cam.proj, cam.view);
+
+    if( oit_active )
+    {
+      // Opaque pass goes here, nothing in this implementation, other than a clear
+      glBindFramebuffer( GL_FRAMEBUFFER, 0 );
+      glDepthMask( GL_TRUE );
+      glClearColor( 1.0f, 0.75f, 0.75f, 1.0f );
+      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+      
+      // Copy depth buffer
+      glBindFramebuffer(GL_READ_FRAMEBUFFER, 0 );
+      glBindFramebuffer(GL_DRAW_FRAMEBUFFER, oit_fbo );
+      glBlitFramebuffer( 0, 0, window_width, window_height, 0, 0, 1024, 720, GL_DEPTH_BUFFER_BIT, GL_NEAREST );
+
+      // OIT pass
+      glBindFramebuffer( GL_FRAMEBUFFER, oit_fbo );
+      // glClear( GL_DEPTH_BUFFER_BIT );
+      glViewport( 0, 0, 1024, 720 );
+      glDepthMask( GL_FALSE );
+
+      static const float clear_accum[] = { 0, 0, 0, 0 };
+      glClearBufferfv( GL_COLOR, 0, clear_accum );
+      glBlendEquationi( 0, GL_FUNC_ADD );
+      glBlendFunci( 0, GL_ONE, GL_ONE );
+
+      static const float clear_reveal[] = { 1,1,1,1 };
+      glClearBufferfv( GL_COLOR, 1, clear_reveal );
+      glBlendEquationi( 1, GL_FUNC_ADD );
+      glBlendFunci( 1, GL_ZERO, GL_ONE_MINUS_SRC_COLOR );
 
 
-    glUseProgram( transparency_program_id );
-    glUniformMatrix4fv( 0, 1, GL_FALSE, &mvp.data[0] );
+      glUseProgram( transparency_program_id );
+      glUniformMatrix4fv( 0, 1, GL_FALSE, &mvp.data[0] );
 
-    glBindVertexArray( quads_vao );
-    glDrawElements( GL_TRIANGLES, 18, GL_UNSIGNED_SHORT, NULL );
+      glBindVertexArray( quads_oit_vao );
+      glDrawElements( GL_TRIANGLES, 18, GL_UNSIGNED_SHORT, NULL );
 
+      // Composite over the default buffer.
+      glBindFramebuffer( GL_FRAMEBUFFER, 0 );
+      glClearColor( 0.75f, 0.75f, 0.75f, 1.0f );
+      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+      glDepthMask( GL_TRUE );
+      glBlendEquation( GL_FUNC_ADD );
+      glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA );
+      glViewport(0, 0, window_width, window_height);
 
+      glActiveTexture(GL_TEXTURE0);
+      glBindTexture(GL_TEXTURE_2D, accum_texture);
 
+      glActiveTexture(GL_TEXTURE1);
+      glBindTexture(GL_TEXTURE_2D, reveal_texture);
 
-    glBindFramebuffer( GL_FRAMEBUFFER, 0 );
-    glDisable( GL_BLEND );
-    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glViewport(0, 0, window_width, window_height);
+      glUseProgram( fullscreen_program_id );
+      glUniform1i( 0, 0 ); // Accum  texture
+      glUniform1i( 1, 1 ); // Reveal texture
+      glBindVertexArray( fullscreen_quad_vao );
+      glDrawArrays( GL_TRIANGLES, 0, 3 );
+    }
+    else
+    {
+      // Draw sorted
+      glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA );
+      glBlendEquation( GL_FUNC_ADD );
 
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, accum_texture);
+      glClearColor( 0.75f, 0.75f, 0.75f, 1.0f );
+      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
 
-    glActiveTexture(GL_TEXTURE1);
-    glBindTexture(GL_TEXTURE_2D, reveal_texture);
+      glUseProgram( sorted_program_id );
+      glUniformMatrix4fv( 0, 1, GL_FALSE, &mvp.data[0] );
 
-    glUseProgram( fullscreen_program_id );
-    glUniform1i( 0, 0 ); // Accum  texture
-    glUniform1i( 1, 1 ); // Reveal texture
-    glBindVertexArray( fullscreen_quad_vao );
-    glDrawArrays( GL_TRIANGLES, 0, 3 );
+      glBindVertexArray( quads_vao );
+      glDrawElements( GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, quads_ind + 6);
+      glDrawElements( GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, quads_ind + 12);
+      glDrawElements( GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, quads_ind );
+    }
 
     glfwSwapBuffers(window);
     glfwPollEvents();
